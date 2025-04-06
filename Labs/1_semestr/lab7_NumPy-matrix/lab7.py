90dfc5864e92f65f79aaed5ac2fcafb86cbd9940
Laptop <user@DESKTOP-85SUUJR>
2025-04-07 01:09:45 +0400

    починил modify и переделал демонстрацию

--- Labs/1_semestr/lab7_NumPy-matrix/lab7.py
+++ Labs/1_semestr/lab7_NumPy-matrix/lab7.py
@@ -7,17 +7,21 @@ class  MyMatrix:
 
     def __str__(self): return str(self.__matrix)
 
-    def getMin(self): return self.__matrix.min()
-    def getMax(self): return self.__matrix.max()
-
-    # найти максимальный элемент и поставить его на первое место.
+    # найти максимальный элемент в столбце и поставить его на первое место.
     def modify(self):
+        # транспонируем для более удобного обхода
         tempM = np.transpose(self.__matrix)
 
         for row in tempM:
-            row[0] = np.max(row)
+            maxIndex = np.argmax(row)
+            temp = row[0]
+            row[0] = row[maxIndex]
+            row[maxIndex] = temp
 
-        self.__matrix = np.transpose(tempM)
+        # транспонируем обратно
+        tempM = np.transpose(tempM)
+
+        self.__matrix = tempM
 
     # C = A.makeC(B)
     # D = A.makeD(B)
@@ -27,38 +31,48 @@ class  MyMatrix:
         t1 = ((np.transpose(B.__matrix) @ self.__matrix) -
               (np.transpose(self.__matrix) @ B.__matrix))
 
-        t2 = (np.transpose(self.__matrix - 6*np.eye(self.__size)))/(B.getMin())
+        t2 = (np.transpose(self.__matrix - 6*np.eye(self.__size)))/(B.__matrix.min())
 
         return t1 + t2
 
     # D = (A + B)^2 / max | Aij | - 8B^2
     def makeD(self, B):
         t1 = (self.__matrix + B.__matrix)**2
-        t1 /= self.getMax()
+        t1 /= self.__matrix.max()
         return t1 - 8*B.__matrix**2
 
 if __name__ == '__main__':
-    m1 = MyMatrix(5, 1, 10)
-    m1_copy = m1
-    m2 = MyMatrix(5, 1, 10)
-    m2_copy = m2
+    m1_original = MyMatrix(5, 1, 10)
+    m2_original = MyMatrix(7, -10, 10)
 
+    m1_copy = m1_original
     print(m1_copy)
-    print('-'*40)
+    print('-'*50)
     m1_copy.modify()
     print(m1_copy)
 
     print()
-    print()
 
+    m2_copy = m2_original
     print(m2_copy)
-    print('-' * 40)
+    print('-'*50)
     m2_copy.modify()
     print(m2_copy)
 
     print()
+    print('#'*50)
     print()
 
-    print(m1.makeC(m2))
-    print(m1.makeD(m2))
+    m3 = MyMatrix(3, 1, 5)
+    m4 = MyMatrix(3, 6, 10)
 
+    print(m3)
+    print()
+    print(m4)
+
+    print()
+    print()
+
+    print(m3.makeC(m4))
+    print()
+    print(m3.makeD(m4))
